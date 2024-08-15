8be308ec35503d8343f417f95281fba1377fb8b2
devbyaccident <blackdenc@chop.edu>
2024-08-14 20:59:54 -0400

    ✨ Wrote subtract function

--- tests.py
+++ tests.py
@@ -1,9 +1,14 @@
 import unittest
 from app import add 
+from app import subtract
 
 class TestDemo(unittest.TestCase):
     def test_add(self):
         self.assertEqual(add(1,1), 2)
+
+    def test_subtract(self):
+        self.assertEqual(subtract(2,1), 1)
+        
 
 if __name__ == '__main__':
     unittest.main()
